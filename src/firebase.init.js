da66b47c71787963598cd114b0c02a84b15f721a
chandan-chakma <cchakma19@gmail.com>
2022-05-05 14:49:43 +0600

    hide auth

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.REACTapiKey,
-    authDomain: process.env.REACTauthDomain,
-    projectId: process.env.REACTprojectId,
-    storageBucket: process.env.REACTstorageBucket,
-    messagingSenderId: process.env.REACTmessagingSenderId,
-    appId: process.env.REACTappId,
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
